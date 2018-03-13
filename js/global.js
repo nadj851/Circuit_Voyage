37c1292072d157b51b1aa16c3734b0e1c589103f
Maredj <e1695946@cmaisonneuve.qc.ca>
2018-03-13 14:38:51 -0400

    ajout : dossier include avec les deux fichies dossier participants avec les fichiers dossier Js avec les fichiers

--- js/global.js
+++ js/global.js
@@ -40,4 +40,30 @@ $(function() {
 				return false;
 		});
 });
+function rendreVisible(elem){
+	document.getElementById(elem).style.display='block';
+}
 
+function rendreInvisible(elem){
+	document.getElementById(elem).style.display='none';
+}
+
+function validerNum(elem){
+	var num=document.getElementById(elem).value;
+	var numRegExp=new RegExp("^[0-9]{1,4}$");
+	if(num!="" && numRegExp.test(num))
+		return true;
+	return false;
+}
+
+function valider(){
+	var num=document.getElementById('num').value;
+	var titre=document.getElementById('titre').value;
+	var duree=document.getElementById('duree').value;
+	var res=document.getElementById('res').value;
+	var numRegExp=new RegExp("^[0-9]{1,4}$");
+	if(num!="" && titre!="" && duree!="" && res!="")
+		if(numRegExp.test(num))
+			return true;
+	return false;
+}
