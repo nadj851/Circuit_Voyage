93268478b9bebe9cbd4cbf96c2f5126dceac5c45
Lala Mostefa Kamal <moustafamosta@hotmail.com>
2018-03-17 00:22:29 -0400

    Initialisation Utilisateurs

---
+++ utilisateurs/utilisateursControleur.php
@@ -0,0 +1,134 @@
+
+<?php
+	require_once("../includes/modele.inc.php");
+	$tabRes=array();
+	function enregistrer(){
+		global $tabRes;	
+		$titre=$_POST['titre'];
+		$duree=$_POST['duree'];
+		$res=$_POST['res'];
+		try{
+			$requete="INSERT INTO participants VALUES(0,?,?,?,?)";
+			$unModele=new filmsModele($requete,array($titre,$duree,$res,$pochete));
+			$stmt=$unModele->executer();
+			$tabRes['action']="enregistrer";
+			$tabRes['msg']="Film bien enregistre";
+		}catch(Exception $e){
+		}finally{
+			unset($unModele);
+		}
+	}
+        //create dynamic  form with jquery php mysql
+        //http://talkerscode.com/webtricks/create-dynamic-form-using-php-jquery-and-mysql.php
+	
+	function lister(){
+		global $tabRes;
+		$tabRes['action']="lister";
+		$requete="SELECT * FROM participants";
+		try{
+			 $unModele=new filmsModele($requete,array());
+			 $stmt=$unModele->executer();
+			 $tabRes['listeFilms']=array();
+			 while($ligne=$stmt->fetch(PDO::FETCH_OBJ)){
+			    $tabRes['listeFilms'][]=$ligne;
+			}
+		}catch(Exception $e){
+		}finally{
+			unset($unModele);
+		}
+	}
+	
+	function enlever(){
+		global $tabRes;	
+		$idf=$_POST['numE'];
+		try{
+			$requete="SELECT * FROM participants WHERE idf=?";
+			$unModele=new filmsModele($requete,array($idf));
+			$stmt=$unModele->executer();
+			if($ligne=$stmt->fetch(PDO::FETCH_OBJ)){
+				$unModele->enleverFichier("pochettes",$ligne->pochette);
+				$requete="DELETE FROM participants WHERE idf=?";
+				$unModele=new filmsModele($requete,array($idf));
+				$stmt=$unModele->executer();
+				$tabRes['action']="enlever";
+				$tabRes['msg']="Film ".$idf." bien enleve";
+			}
+			else{
+				$tabRes['action']="enlever";
+				$tabRes['msg']="Film ".$idf." introuvable";
+			}
+		}catch(Exception $e){
+		}finally{
+			unset($unModele);
+		}
+	}
+	//methode fiche
+	function fiche(){
+		global $tabRes;
+		$idf=$_POST['numF'];
+		$tabRes['action']="fiche";
+		$requete="SELECT * FROM participants WHERE idf=?";
+		try{
+			 $unModele=new filmsModele($requete,array($idf));
+			 $stmt=$unModele->executer();
+			 $tabRes['fiche']=array();
+			 if($ligne=$stmt->fetch(PDO::FETCH_OBJ)){
+			    $tabRes['fiche']=$ligne;
+				$tabRes['OK']=true;
+			}
+			else{
+				$tabRes['OK']=false;
+			}
+		}catch(Exception $e){
+		}finally{
+			unset($unModele);
+		}
+	}
+	
+	function modifier(){
+		global $tabRes;	
+		$titre=$_POST['titreF'];
+		$duree=$_POST['dureeF'];
+		$res=$_POST['resF'];
+		$idf=$_POST['idf']; 
+		try{
+			//Recuperer ancienne pochette
+			$requette="SELECT pochette FROM participants WHERE idf=?";
+			$unModele=new filmsModele($requette,array($idf));
+			$stmt=$unModele->executer();
+			$ligne=$stmt->fetch(PDO::FETCH_OBJ);
+			$anciennePochette=$ligne->pochette;
+			$pochette=$unModele->verserFichier("pochettes", "pochette",$anciennePochette,$titre);	
+			
+			$requete="UPDATE participants SET titre=?,duree=?, res=?, pochette=? WHERE idf=?";
+			$unModele=new filmsModele($requete,array($titre,$duree,$res,$pochette,$idf));
+			$stmt=$unModele->executer();
+			$tabRes['action']="modifier";
+			$tabRes['msg']="Film $idf bien modifie";
+		}catch(Exception $e){
+		}finally{
+			unset($unModele);
+		}
+	}
+	//******************************************************
+	//Contr�leur
+	$action=$_POST['action'];
+	switch($action){
+		case "enregistrer" :
+			enregistrer();
+		break;
+		case "lister" :
+			lister();
+		break;
+		case "enlever" :
+			enlever();
+		break;
+		case "fiche" :
+			fiche();
+		break;
+		case "modifier" :
+			modifier();
+		break;
+	}
+    echo json_encode($tabRes);
+?>
