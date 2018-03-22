162edc9c1483a7e3a8593a55ed1f4fbcab906b0b
Lala Mostefa Kamal <moustafamosta@hotmail.com>
2018-03-22 17:10:34 -0400

    Modification d'un circuit presque complète

--- Circuit/CircuitAPIJQuery.js
+++ Circuit/CircuitAPIJQuery.js
@@ -3,14 +3,14 @@ function AfficherCircuits(idThem) {
     formCircuit.append('action', 'enregistrerCircuit');
     $.ajax({
         type: 'GET',
-        url: 'Circuit/CircuitAPI.php?idThem='+idThem,
+        url: 'Circuit/CircuitAPI.php?idThem=' + idThem,
         dataType: 'json', //text pour le voir en format de string
         //async : false,
         //cache : false,
         contentType: false,
         processData: false,
-        success: function (reponse) {//alert(reponse);
-           //Affichage
+        success: function (reponse) {//alert(reponse.affichageCircuits[0].idCircuit);
+            //Affichage
             AffichageCircuit(reponse.affichageCircuits);
         },
         fail: function (err) {
@@ -19,27 +19,30 @@ function AfficherCircuits(idThem) {
     });
 }
 
-function AffichageCircuit(affichageCircuits){
-    alert(affichageCircuits.idCircuit);
-    
-    /*
+function AffichageCircuit(affichageCircuits) {
     $('#get_result').html("");
-    
-    
-    taille = listetheme.length;
-    var result = "";
 
+
+    taille = affichageCircuits.length;
+    var result = "";
     for (var i = 0; i < taille; i++) {
         result += "<div class='col-md-4'>";
         result += "<div class='panel panel-success'>";
-        result += "<div class='panel-heading'>" + "<span style='font-weight: bold;font-size: 15px;'>" + listetheme[i].nom + "</span></div>";
+        result += "<div class='panel-heading'>" + "<span style='font-weight: bold;font-size: 15px;'>" + affichageCircuits[i].titre + "</span></div>";
         result += "<div class='panel-body'>";
-        result += "<a href='#' name='afficherCircuit'  id='" + listetheme[i].idThematique + "' class=\"thumbnail\">	<img src='pochettes/" + listetheme[i].image + "' style='width:400px; height:300px;'></a>";
+        result += "<a name='afficherDetailsCircuit'  id='" + affichageCircuits[i].idCircuit + "' class=\"thumbnail\">	<img src='pochettes/" + affichageCircuits[i].imageCircuit + "' style='width:400px; height:300px;'></a>";
+        result += "<div> Description: " + affichageCircuits[i].description + " <br></div>";
+        result += "<div> Prix: " + affichageCircuits[i].prix + " <br></div>";
         result += "</div>";
         result += "</div>";
         result += "</div>";
     }
     $('#get_result').html(result);
-    */
+
+    $("a[name=afficherCircuit]").click(function () {
+        //AfficherDetailsCircuits($(this).prop("id"));
+    });
+
+
 }
 
