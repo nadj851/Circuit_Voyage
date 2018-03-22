df141d841bc615ab6914e74fa0fae027f7777a6f
e1695760 <e1695760@FC413-G3HSX12.cmaisonneuve.qc.ca>
2018-03-22 18:16:08 -0400

    Maintenant chaque circuit est capable de repondre au click et afficher sa propre page de details. L'API Circuits est complet: il affiche tous les circuits par defaut, par id si idCircuit est specifié dans l'URL ou par Id de la Thematique si idThem est specifé. Cet API et aussi les balisses <a> marquées avec name=afficherCircuit ou name=afficherDetailsCircuit peuvent etre utilisées pour naviger partout dans le site.

--- Circuit/CircuitAPI.php
+++ Circuit/CircuitAPI.php
@@ -1,22 +1,38 @@
 <?php
+
 require_once("../includes/init.php");
 require_once("../includes/modele.inc.php");
 $tabRes = array();
-    global $tabRes;
-    $idThem = $_GET['idThem'];
-    $tabRes['action'] = "afficherCircuits";
-    $requete = "SELECT * FROM circuit WHERE idThematique=?";
-    try {
-        $unModele = new filmsModele($requete, array($idThem));
-        $stmt = $unModele->executer();
-        $tabRes['affichageCircuits'] = array();
-        while ($ligne = $stmt->fetch(PDO::FETCH_OBJ)) {
-            $tabRes['affichageCircuits'][] = $ligne;
-        }
-    } catch (Exception $e) {
-        $tabRes['affichageCircuits'][] = $e;
-    } finally {
-        unset($unModele);
+global $tabRes;
+$requete = "SELECT * FROM circuit ";
+$tabRes['action'] = "afficherCircuits";
+$param = "";
+
+if (isset($_GET['idCircuit'])) {
+    $param = $_GET['idCircuit'];
+    $requete=$requete." WHERE idCircuit=?";
+}
+else if (isset($_GET['idThem'])) {
+    $param = $_GET['idThem'];
+    $requete=$requete." WHERE idThematique=?";
+} 
+
+$params = array();
+if ($param !== "") {
+    $params = array($param);
+}
+
+try {
+    $unModele = new filmsModele($requete, $params);
+    $stmt = $unModele->executer();
+    $tabRes['affichageCircuits'] = array();
+    while ($ligne = $stmt->fetch(PDO::FETCH_OBJ)) {
+        $tabRes['affichageCircuits'][] = $ligne;
     }
+} catch (Exception $e) {
+    $tabRes['affichageCircuits'][] = $e;
+} finally {
+    unset($unModele);
+}
 echo json_encode($tabRes);
 
