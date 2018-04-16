a94929428fa5a74fff7b293f2a35cf1edf39646d
NADJIB <e1695861@FC410-8SG0JB2.cmaisonneuve.qc.ca>
2018-04-16 18:54:27 -0400

    un API Android

--- phpAndroidApi/ControleuAndroid.php
+++ phpAndroidApi/ControleuAndroid.php
@@ -2,6 +2,15 @@
 	require_once("../includes/modele.inc.php");
 require_once("../includes/init.php");
 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+if (isset($_GET['idCircuit'])) {
+    $_SESSION["idCircuit"]=$_GET['idCircuit'];
+    ficheCircuit();
+} else if (isset($_GET['idThem'])) {
+    listerCircuitParThematique();
+}
 
 $tabRes = array();
 ///////////////////////utilisateur/////////////////////////////////////
@@ -178,7 +187,230 @@ function getListThematique(){
 
  ////////////////Circuit///////////////////////   
 
+function enregistrerCircuit()
+{
+    global $tabRes;
 
+    $nomCircuit = $_POST['nomCircuit'];
+    $dateDepart = $_POST['dateDepartCircuit'];
+    $dateRetour = $_POST['dateRetourCircuit'];
+    $nbPersonnesMax = $_POST['nbPersonneMax'];
+    $nbPersonnesMin = $_POST['nbPersonneMin'];
+    $description = $_POST['desc'];
+    $prix = $_POST['prixCircuit'];
+    $idthematique = $_POST['themeCircuit'];
+    $imageCircuit = $_POST['imageCircuit'];
+    $guide = $_POST['guide'];
+    $idPromo = $_POST['idPromo'];
+
+    try {
+        $unModele = new circuitModel();
+        $pochete = $unModele->verserFichier("pochettes", "pochette", "avatar.jpg", $nomCircuit);
+        $requete = "INSERT INTO circuit VALUES(0,?,?,?,?,?,?,?,?,?,?,?,?)";
+        $unModele = new circuitModel($requete, array($nomCircuit, $dateDepart, $dateRetour, $nbPersonnesMax, $nbPersonnesMin, $description, $prix, $pochete, null, null, $idthematique,0));
+        $stmt = $unModele->executer();
+        $_SESSION["idCircuit"] = $unModele->lastID;
+        $tabRes['action'] = "enregistrer";
+        $tabRes['msg'] = "circuit bien enregistrer";
+    } catch (Exception $e) {
+        echo $e;
+    } finally {
+        echo json_encode($tabRes);
+        unset($unModele);
+    }
+}
+
+function listerLesCircuit()
+{
+    global $tabRes;
+    $tabRes['action'] = "listerLesCircuits";
+    $requete = "SELECT * FROM circuit";
+    try {
+        $unModele = new circuitModel($requete, []);
+        $stmt = $unModele->executer();
+        $tabRes['listecircuit'] = [];
+        while ($ligne = $stmt->fetch(PDO::FETCH_OBJ)) {
+            $tabRes['listecircuit'][] = $ligne;
+        }
+    } catch (Exception $e) {
+        echo $e;
+    } finally {
+        echo json_encode($tabRes);
+        unset($unModele);
+    }
+}
+
+function listerCircuitParThematique()
+{
+    global $tabRes;
+    $tabRes['action'] = "lister";
+    $requete = "SELECT * FROM circuit WHERE idThematique=?";
+    try {
+        $unModele = new circuitModel($requete, array($_GET['idThem']));
+        $stmt = $unModele->executer();
+        $tabRes['listeCircuits'] = array();
+        while ($ligne = $stmt->fetch(PDO::FETCH_OBJ)) {
+            $tabRes['listeCircuits'][] = $ligne;
+        }
+    } catch (Exception $e) {
+        echo $e;
+    } finally {
+        echo json_encode($tabRes);
+        unset($unModele);
+    }
+    echo json_encode($tabRes);
+}
+
+function ficheCircuit()
+{
+    global $tabRes;
+    include '../Thematique/ThematiqueControleur.php';
+
+    $idCircuit;
+    if (isset($_POST['idCircuit'])) {
+        $idCircuit = $_POST['idCircuit'];
+        listerThematique();
+    }
+    if (isset($_GET['idCircuit'])) {
+        $idCircuit = $_GET['idCircuit'];
+    }
+
+    $requete = "SELECT * FROM circuit WHERE idCircuit=?";
+    try {
+        $unModele = new circuitModel($requete, array($idCircuit));
+        $stmt = $unModele->executer();
+        $tabRes['ficheCircuit'] = array();
+        if ($ligne = $stmt->fetch(PDO::FETCH_OBJ)) {
+            $tabRes['ficheCircuit'][] = $ligne;
+            $tabRes['OK'] = true;
+            $tabRes['action'] = "afficherFiche";
+        } else {
+            $tabRes['OK'] = false;
+        }
+    } catch (Exception $e) {
+        echo $e;
+    } finally {
+        echo json_encode($tabRes);
+        unset($unModele);
+    }
+    echo json_encode($tabRes);
+    
+
+}
+
+function modifierCircuit()
+{
+    global $tabRes;
+    $idCircuit = $_POST['idCircuit'];
+    $nomCircuit = $_POST['nomCircuit'];
+    $dateDepart = $_POST['dateDepartCircuit'];
+    $dateRetour = $_POST['dateRetourCircuit'];
+    $nbPersonnesMax = $_POST['nbPersonneMax'];
+    $nbPersonnesMin = $_POST['nbPersonneMin'];
+    $description = $_POST['descripCircuit'];
+    $prix = $_POST['prixCircuit'];
+    //$imageCircuit = $_POST['imageCircuit'];
+    $guide = $_POST['guide'];
+    $idPromo = $_POST['idPromo'];
+    $idthematique = $_POST['themeCircuit'];
+
+    try {
+        //Recuperer ancienne pochette
+        $requette = "SELECT imageCircuit FROM circuit WHERE idCircuit=?";
+        $unModele = new circuitModel($requette, array($idCircuit));
+        $stmt = $unModele->executer();
+        $ligne = $stmt->fetch(PDO::FETCH_OBJ);
+        $anciennePochette = $ligne->imageCircuit;
+        $imageCircuit = $unModele->verserFichier("pochettes", "imageCircuit", $anciennePochette, $nomCircuit);
+
+        $requete = "UPDATE circuit SET titre=?,dateDeDepart=?, dateDeRetour=?, nbPersonnesMax=?,"
+            . " nbPersonnesMin=?, description=?, prix=?, imageCircuit=?,"
+            . " guide=?, idPromotion=?, idThematique=? WHERE idCircuit=?";
+        $unModele = new circuitModel($requete, array($nomCircuit, $dateDepart, $dateRetour, $nbPersonnesMax, $nbPersonnesMin,
+            $description, $prix, $imageCircuit, $guide, $idPromo, $idthematique, $idCircuit));
+        $stmt = $unModele->executer();
+        $tabRes['action'] = "modifier";
+        $tabRes['msg'] = "Circuit $idCircuit bien modifie";
+    } catch (Exception $e) {
+        print_r($e);
+    } finally {
+        echo json_encode($tabRes);
+        unset($unModele);
+    }
+}
+////////////////////etape////////////////////////////////
+function enregistrerEtape() {
+    global $tabRes;
+   
+    $nomEtape = $_POST['nomEtape'];
+    $descriptionEtape = $_POST['descE'];
+    
+  
+   
+    try {
+          $idCircuit=$_SESSION["idCircuit"];
+        $unModele = new circuitModel();
+        //$pochete = $unModele->verserFichier("pochettes", "pochette", "avatar.jpg", $nomCircuit);
+        $requete = "INSERT INTO etape VALUES(0,?,?,?)";
+        $unModele = new circuitModel($requete, array($nomEtape,$descriptionEtape,$idCircuit));
+        $stmt = $unModele->executer();
+        $_SESSION["idEtape"]=$unModele->lastID;
+       
+        $tabRes['action'] = "enregistrer";
+       // $tabRes['idCircuit'] = $idCircuit;
+        $tabRes['msg'] = "étape bien enregistrer";
+    } catch (Exception $e) {
+        echo $e;
+    } finally {
+         echo json_encode($tabRes);
+        unset($unModele);
+    }
+}
+
+
+////////////////////jour////////////////////////////
+function enregistrerJour() {
+    global $tabRes;
+   
+    $nomHotel = $_POST['nomHotel'];
+    $urlHotel = $_POST['UrlHotel'];
+    $nomRestaurant = $_POST['nomRestaurant'];
+    $urlRestaurant = $_POST['urlRestaurant'];
+    $dejeuner = 0;
+    $diner = 0;
+    $souper = 0;
+    $descripJour = $_POST['descJ'];
+    $idEtape=$_SESSION["idEtape"];
+
+    if (isset($_POST['dejeuner'])) {
+        $dejeuner=1;
+    }
+    if (isset($_POST['diner'])) {
+        $diner=1;
+    }
+    if (isset($_POST['souper'])) {
+        $souper=1;
+    }
+    
+    try {
+          $idCircuit=$_SESSION["idCircuit"];
+        $unModele = new circuitModel();
+        //$pochete = $unModele->verserFichier("pochettes", "pochette", "avatar.jpg", $nomCircuit);
+        $requete = "INSERT INTO jour VALUES(?,?,?,?,?,?,?,?,?)";
+        $unModele = new circuitModel($requete, array($nomHotel,$urlHotel,$nomRestaurant,$urlRestaurant,$dejeuner,
+            $diner,$souper,$descripJour,$idEtape));
+        $stmt = $unModele->executer();
+        
+        $tabRes['action'] = "enregistrer";
+        $tabRes['idCircuit'] = $idCircuit;
+        $tabRes['msg'] = "Jour bien enregistrer";
+    } catch (Exception $e) {
+        echo $e;
+    } finally {
+         echo json_encode($tabRes);
+        unset($unModele);
+    }
+}
 
 //******************************************************
 //Contr�leur
@@ -229,6 +461,16 @@ switch ($action) {
             modifierCircuit();
           
             break;
+        
+        //////etape////////////////
+         case "enregistrerEtapeE" :
+        enregistrerEtape();
+        break;
+    ///////////////jours//////////////
+      case "enregistrerJourJ" :
+        enregistrerJour();
+        break;
+    
     }
         
         
