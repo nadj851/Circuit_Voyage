59fd0f1942bacc736f3b02c43bcdc8c2cf138689
e1695760 <e1695760@FC413-G3CHX12.cmaisonneuve.qc.ca>
2018-04-16 12:55:31 -0400

    Internalization

--- Index.php
+++ Index.php
@@ -56,14 +56,13 @@ include_once 'includes/init.php';
         <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.js'></script>
         <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
-        <style>   
-            /*
+        <style>            
             .goog-te-banner-frame.skiptranslate {
                 display: none !important;
             } 
             body {
                 top: 0px !important; 
-            }*/
+            }
         </style>   
 
 
